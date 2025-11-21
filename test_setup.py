30daa41b4462efac86f684aceb370001c3b7297d
Max Lovell <max_lovell@hotmail.co.uk>
2025-11-21 12:48:42 +0000

    Add test setup script

---
+++ test_setup.py
@@ -0,0 +1,7 @@
+import sys
+import pandas as pd
+import paramiko
+
+print(f"Python Version: {sys.version}")
+print(f"Pandas Version: {pd.__version__}")
+print(f"Paramiko Version: {paramiko.__version__}")
